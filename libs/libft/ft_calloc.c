c305d05e41e4681296b02016906b395b4ff1d4d8
Aline Queiroz <alinequeirozagq.com>
2023-04-10 22:56:42 -0300

    ft_calloc refactor

--- libs/libft/ft_calloc.c
+++ libs/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aqueiroz <aqueiroz@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 21:20:25 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/10 22:54:31 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/04/10 22:56:36 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	tot_size = nmemb * size;
 	if (nmemb == 0 || size == 0 || tot_size > INT_MAX)
 		return (NULL);
-
 	pos = malloc(tot_size);
 	if (pos == NULL)
 		return (NULL);
-
 	memset(pos, 0, tot_size);
 	return (pos);
 }
-
