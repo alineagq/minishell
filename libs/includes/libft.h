bf94102156cc5376860a55297a784c88b4e33cfe
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2023-04-21 22:52:44 +0000

    fix t_islnum error

--- libs/includes/libft.h
+++ libs/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:19:53 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/21 22:33:54 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/04/21 22:48:43 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ int			ft_isalpha(char c);
 int			ft_isdigit(int is_a_digit);
 int			ft_isascii(int is_a_ascii);
 int         ft_isalnum(int is_a_alnum);
+
 #endif
