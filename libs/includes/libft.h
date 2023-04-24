304ebc73a257ebd39a0405b97532a0a6a1cd9119
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2023-04-24 21:03:05 +0000

    t_strlen. test waiting for approval

--- libs/includes/libft.h
+++ libs/includes/libft.h
@@ -6,17 +6,19 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:19:53 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/24 20:36:40 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/04/24 20:57:29 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H 
+# define LIBFT_H
+#include <string.h>
 
 int     ft_isalpha(char c_is_alpha);
 int		ft_isdigit(int c_is_a_digit);
 int		ft_isascii(int c_is_a_ascii);
 int     ft_isalnum(int c_is_a_alnum);
 int     ft_isprint(int c_is_printable);
+size_t  ft_strlen(const char *str);
 
 #endif
