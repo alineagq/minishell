e2dcc15051ec73c3e2f1ad33ce682b1638d6d624
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2023-04-24 20:35:14 +0000

    test and function isprint

--- libs/includes/libft.h
+++ libs/includes/libft.h
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:19:53 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/21 22:48:43 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/04/24 20:28:19 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H 
 
-int			ft_isalpha(char c);
-int			ft_isdigit(int is_a_digit);
-int			ft_isascii(int is_a_ascii);
-int         ft_isalnum(int is_a_alnum);
+int     ft_isalpha(char c);
+int		ft_isdigit(int is_a_digit);
+int		ft_isascii(int is_a_ascii);
+int     ft_isalnum(int is_a_alnum);
+int     ft_isprint(int is_printable);
 
 #endif
