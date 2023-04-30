3dfcbfc1117caca8af13fc98688774b5b64e18ad
fsuomins <fsuomins@student.42sp.org.br>
2023-04-29 21:50:26 -0300

    fixed Makefile changes

--- libs/ft_isalnum.c
+++ libs/ft_isalnum.c
@@ -6,15 +6,15 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 22:28:10 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/24 21:35:44 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/04/29 21:47:02 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../includes/libft.h"
 
-int ft_isalnum(int c_is_a_alnum)
+int ft_isalnum(int is_a_alnum)
 {
-    if (ft_isalpha(c_is_a_alnum) || ft_isdigit(c_is_a_alnum))
+    if (ft_isalpha(is_a_alnum) || ft_isdigit(is_a_alnum))
         return (1);
     return (0);
 }
--- libs/srcs/ft_isdigit.c
+++ libs/srcs/ft_isdigit.c
@@ -1,16 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 10:40:31 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/04/28 10:40:33 by fsuomins         ###   ########.fr       */
+/*   Created: 2023/04/29 20:48:19 by fsuomins          #+#    #+#             */
+/*   Updated: 2023/04/29 20:48:26 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/libft.h"
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-#endif
+int	ft_isdigit(int is_a_digit)
+{
+	if (is_a_digit >= '0' && is_a_digit <= '9')
+		return (1);
+	return (0);
+}
