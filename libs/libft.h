a3e94e4d33d966860ec78592a69eaae733e1ca6b
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2023-05-09 20:51:55 +0000

    fix make test

--- libs/libft.h
+++ libs/libft.h
@@ -6,7 +6,7 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:19:53 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/05/09 20:01:59 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:45:32 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int		ft_isascii(int c_is_a_ascii);
 int     ft_isalnum(int c_is_a_alnum);
 int     ft_isprint(int c_is_printable);
 size_t  ft_strlen(const char *str);
+void *ft_memset(void *ptr, int value, size_t num);
 
 #endif
