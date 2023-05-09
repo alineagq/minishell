cf865fae0c0a36172ca94d741205d393dd0ea0e6
FERNANDA SUOMINSKY <82189986+Ferskys@users.noreply.github.com>
2023-05-09 20:52:47 +0000

    fix make test

---
+++ libs/srcs/ft_isalpha.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 11:47:37 by aqueiroz          #+#    #+#             */
+/*   Updated: 2023/05/09 20:01:44 by fsuomins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+/* THIS FUNCTION is ... I 
+
+
+if you want try ascii using decimal: (c_is_a_alpha >= 97  ||  c_is_a_alpha 
+<= 122), "is the same thing! ;-) */
+
+int	ft_isalpha(int c_is_a_alpha)
+{
+	if (c_is_a_alpha >= 'a' && c_is_a_alpha <= 'z')
+		return (1);
+	else if (c_is_a_alpha >= 'A' && c_is_a_alpha <= 'Z')
+		return (1);
+	return (0);
+}
