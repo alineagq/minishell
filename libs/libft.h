bbf3fe0c6b00a16509de5766625d5e4b4e41d9a8
Fsuomins <fsuomins@student.42sp.org.br>
2023-06-18 17:00:19 -0300

    feature/2(test/lib): ft_memchr.c

--- libs/libft.h
+++ libs/libft.h
@@ -6,7 +6,7 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:19:53 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/06/18 16:10:13 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/06/18 16:53:26 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@
 # include <stdlib.h>
 # define SIZE_MAX 2147483647
 
-size_t	ft_strlen(const char *str);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-void	*ft_memset(void *ptr, int value, size_t num);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *str);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *ptr, int value, size_t num);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
-char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_isprint(int c_is_printable);
 int		ft_isalpha(int c_is_a_alpha);
