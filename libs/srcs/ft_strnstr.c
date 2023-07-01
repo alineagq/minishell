eb51f1cd34553c0f37c309918b75c9e63889c126
Aline Queiroz <alinequeiroz@Alines-MacBook-Pro.local>
2023-07-01 12:35:32 -0300

    lint(norminette): fixes norm lint errors.

--- libs/srcs/ft_strnstr.c
+++ libs/srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 02:32:24 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/07/01 12:22:25 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/07/01 12:33:41 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	while (*haystack && len--)
 	{
-
 		h = haystack;
 		n = needle;
-
 		while (*h && *n && *h == *n && len--)
 		{
 			h++;
 			n++;
 		}
-
 		if (*n == '\0')
 			return ((char *)haystack);
-
 		haystack++;
 		len++;
 	}
-
 	return (NULL);
 }
