a48ef2aa0e3f422ca0e0b6eb9d18e9a23a758cb3
Fsuomins <fsuomins@student.42sp.org.br>
2023-07-02 11:06:36 -0300

    feat(strdup): created t_strdup.

--- libs/tests/t_strdup.c
+++ libs/tests/t_strdup.c
@@ -6,7 +6,7 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 22:05:21 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/07/02 10:49:39 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/07/02 10:56:23 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,24 @@ void    t_strdup(void)
     int counter = 1;
 	
     printf("> Testing: %s\n", __FILE__);
+
+    // Test case 1: Non-empty string
+    const char *str1 = "Hello, World!";
+    char *result1 = ft_strdup(str1);
+    assert(strcmp(result1, str1) == 0);
+    free(result1);
+
+    // Test case 2: Empty string
+    const char *str2 = "";
+    char *result2 = ft_strdup(str2);
+    assert(strcmp(result2, str2) == 0);
+    free(result2);
+
+    // Test case 3: String with special characters
+    const char *str3 = "Testing special characters: !@#$%^&*()";
+    char *result3 = ft_strdup(str3);
+    assert(strcmp(result3, str3) == 0);
+    free(result3);
 
     if (arraySize != 0)
         printf("\n");
